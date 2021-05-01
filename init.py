6406de518aac0343512c14a3bdb5817cc7476f2a
avslav <66104364+avslav@users.noreply.github.com>
2021-05-01 11:43:05 -0400

    Update and rename pyautojava.py to init.py

---
+++ init.py
@@ -0,0 +1,68 @@
+import Variables.makevar
+from Variables.makevar import MakeVar
+
+class javaInit:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def initJava(self, filename: str, *, create_main:bool=True):
+        
+        fullFileName = filename + ".java"
+        fullFileName.strip()
+
+        
+        with open(fullFileName, "w") as jf:
+
+            if create_main:
+            
+                jf.write("public class {}".format(filename))
+                jf.write("{")
+                jf.write("\n public static void main(String[] args) {")
+            
+            else:
+                jf.write("public class {}".format(filename))
+                jf.write("{")
+
+            jf.close()
+        
+        return print(f"Created and initialized file {filename}.java successfully!")
+
+    def closeJava(self, filename: str, *, close_main: bool):
+
+
+        fullFileName = filename + ".java"
+        fullFileName.strip()
+        
+        with open(fullFileName, "a") as jf:
+
+            if close_main:
+                jf.write("}")
+                jf.write("}")
+                print(f"Closed the superclass and the main function in file {filename}.java successfully!")
+        
+            else:
+                jf.write("}")
+                print(f"Closed the superclass in file {filename}.java successfully!")
+        
+            jf.close()
+        
+        
+    def printJava(self, args):
+
+        try: 
+            if filename.endswith(".java"):
+                fullFileName = self.filename
+            else:
+                fullFileName = filename + ".java"
+                fullFileName.strip()
+        
+            with open(fullFileName, "a") as jf:
+            
+                jf.write(f"System.out.println(\"{args}\");")
+
+            print("Added: System.out.printin({}) to your Java file!".format(args))
+        
+        except Exception as e:
+            print(e)
+
+   
