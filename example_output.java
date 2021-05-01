16a9787c2ad7cbc1721a770eb14065008585e89a
avslav <66104364+avslav@users.noreply.github.com>
2021-05-01 11:44:40 -0400

    Create example_output.java

---
+++ example_output.java
@@ -0,0 +1,15 @@
+public class example_output{
+ public static void main(String[] args) {
+String testStr = "this is a string";
+
+int testInt = 60;
+
+boolean testBool = true;
+
+float testFloat = 2.0f;
+
+double testDouble = 6.5;
+
+char testChar = 'A';
+
+ }}
